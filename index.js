4bdbcf75cd2f452987386e4950476ab9ce000caa
Jyoti Ranjan Biswal <jranjan2017@gmail.com>
2022-10-11 13:39:02 +0530

    sub string

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 
-function strLength(str1, str2) {
-    let a = str1.length, b = str2.length;
-    return a+b;
+// function strLength(str1, str2) {
+//     let a = str1.length, b = str2.length;
+//     return a+b;
+// }
+// console.log(strLength('jyoti', 'ranjan'))
+
+function subStrChecker(mainString, subString) {
+    let str1 = mainString, str2 = subString;
+    let count = str2.length;  //2
+    // let j = count, i = 0;
+    let j = count, i=0;
+    while(j<str1.length+1) {
+        if(str2==str1.slice(i, j)){
+            return true
+        }
+        i++
+        j++
+    }
 }
-console.log(strLength('jyoti', 'ranjan'))
+
+let a = subStrChecker('jyotiranjan', 'ran') //2
+console.log(a)
